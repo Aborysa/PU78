29bc8edd356d6a3bc0aa99421444b538f4aa00ec
margreli <margreli@stud.ntnu.no>
2017-03-01 14:57:42 +0100

    added event router to api

--- server/src/api/index.js
+++ server/src/api/index.js
@@ -3,9 +3,12 @@
 let api = require('./api.js');
 
 let feide = require('./feide.js');
+let event = require('./event.js');
 
 //let r_odf = require('./odf.js');
 
 api.api.use(feide);
+api.api.use(event);
 
 module.exports = api;
+
