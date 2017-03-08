ad0ef3586aa4d7d1a5108109d5d7e75d7f0f110e
Jakob <jakob@westermoen.com>
2017-03-08 16:32:30 +0100

    fixed viewEventModal popup for onClick on events, also embeded mazeMap in the modal

--- client/app/src/calendar.jsx
+++ client/app/src/calendar.jsx
@@ -18,8 +18,27 @@ export class CalendarView extends React.Component{
   constructor(props) {
     super(props);
     this.state = {
-      events: []
+      events: [],
+      viewModalProps: {
+        show: false
+      }
     }
+  }
+
+  openViewEventModal(event){
+    this.setState(Object.assign(this.state,{
+      viewModalProps: {
+        show: true,
+        event: event
+      }
+    }));
+  }
+  closeViewModal(){
+    this.setState(Object.assign(this.state,{
+      viewModalProps: {
+        show: false
+      }
+    }));
   }
   componentDidMount(){
     eventService.getEvents().subscribe(events => {
@@ -30,7 +49,10 @@ export class CalendarView extends React.Component{
 
   }
 
+
   render() {
+    let viewEventModal = <ViewEventModal show={this.state.viewModalProps.show} event={this.state.viewModalProps.event}/>
+
     return (
       <div>
         <Col xs={12} md={10} mdOffset={1}>
@@ -41,12 +63,13 @@ export class CalendarView extends React.Component{
              defaultView={'week'}
              popup={true}
              scrollToTime={starttime}
-             onSelectEvent={event => (alert(event.desc))}
+             onSelectEvent={(e) => this.openViewEventModal(e)}
              onSelectSlot={(slotInfo) => alert(
               `selected slot: \n\nstart ${slotInfo.start.toLocaleString()} ` +
               `\nend: ${slotInfo.end.toLocaleString()}`)}
           />
-          <AddEventModal/>
+          <AddEventModal />
+          {viewEventModal}
         </Col>
         <Col xs={12} md={10} mdOffset={1}>
           <hr className="sepCals"/>
@@ -78,9 +101,23 @@ class AddEventModal extends React.Component{
     this.state = this.getInitialState();
     this.pstart = new Date();
     this.pend = new Date();
+    this.handleInputChange = this.handleInputChange.bind(this);
   }
   getInitialState() {
-    return { showModal: false };
+    return {
+      isRepete: false,
+      showModal: false
+     };
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
   }
 
   close() {
@@ -104,6 +141,16 @@ class AddEventModal extends React.Component{
     this.close();
   }
   render() {
+    let endRepete = this.state.isRepete ?
+      <FormGroup controlId="formHorizontalRepeteEnd">
+        <Col componentClass={ControlLabel} sm={2}>
+          Avslutt repetisjon
+        </Col>
+        <Col sm={4}>
+          <Datetime onChange={(v) => this.end = v} ref="repetitionEnd" />
+        </Col>
+      </FormGroup> :
+      null;
 
     return (
       <div>
@@ -120,7 +167,7 @@ class AddEventModal extends React.Component{
                   Tittel
                 </Col>
                 <Col sm={10}>
-                  <FormControl ref="title" type="text" placeholder="Tittel" />
+                  <FormControl ref="title" type="text" placeholder="Tittel"/>
                 </Col>
               </FormGroup>
               <FormGroup controlId="formHorizontalDescription">
@@ -135,27 +182,36 @@ class AddEventModal extends React.Component{
                 <Col componentClass={ControlLabel} sm={2}>
                   Starttid
                 </Col>
-                <Col sm={10}>
-                  <Col sm={6}>
-                    <Datetime onChange={(v) => this.start = v} ref="start" />
-                  </Col>
+                <Col sm={4}>
+                  <Datetime onChange={(v) => this.start = v} ref="start" readOnly={true}/>
                 </Col>
               </FormGroup>
               <FormGroup controlId="formHorizontalEndDate">
                 <Col componentClass={ControlLabel} sm={2}>
                   Sluttid
                 </Col>
-                <Col sm={10}>
-                  <Col sm={6}>
-                    <Datetime onChange={(v) => this.end = v} ref="end" />
-                  </Col>
+                <Col sm={4}>
+                  <Datetime onChange={(v) => this.end = v} ref="end" />
                 </Col>
               </FormGroup>
+              <FormGroup controlId="formHorizontalRepeteToggle">
+                <Col componentClass={ControlLabel} sm={2}>
+                  Ukentlig repetisjon
+                </Col>
+                <Col sm={1}>
+                  <input
+                    name="isRepete"
+                    type="checkbox"
+                    checked={this.state.isRepete}
+                    onChange={this.handleInputChange} />
+                </Col>
+              </FormGroup>
+              {endRepete}
             </Form>
           </Modal.Body>
           <Modal.Footer>
-            <Button onClick={() => this.saveClick()} bsStyle="success">Save</Button>
-            <Button onClick={() => this.close()} bsStyle="primary">Close</Button>
+            <Button onClick={() => this.saveClick()} bsStyle="success">Lagre</Button>
+            <Button onClick={() => this.close()} bsStyle="primary">Lukk</Button>
           </Modal.Footer>
         </Modal>
       </div>
@@ -167,44 +223,60 @@ class ViewEventModal extends React.Component{
   constructor(props){
     super(props);
     this.state = this.getInitialState();
-    this.pstart = new Date();
-    this.pend = new Date();
-    this.title = ReactDOM.findDOMNode(this.refs.title).value;
-    this.desc = ReactDOM.findDOMNode(this.refs.desc).value;
   }
-  getInitialState() {
-    return { showModal: false };
+  getInitialState(){
+    return { showEvent: false };
+  }
+  close(){
+    this.setState({ showEvent: false });
+  }
+  open(){
+    this.setState({ showEvent: true });
+  }
+  toggle(){
+    this.setState({ showEvent: !this.state.showEvent });
   }
 
-  close() {
-    this.setState({ showModal: false });
+  componentWillReceiveProps(props){
+    this.setState({ showEvent: props.show});
   }
-  set start(v){
-    this.pstart = v;
-  }
-  set end(v){
-    this.pend = v;
-  }
-  open() {
-    this.setState({ showModal: true });
+
+  componentDidMount(){
   }
 
   render() {
-
     return (
       <div>
-        <Button></Button>
-        <Modal show={this.state.showModal} onHide={() => this.close()}>
+        <Modal show={this.state.showEvent} onHide={() => this.close()}>
           <Modal.Header closeButton>
-            <Modal.Title>{title}</Modal.Title>
+            <h1>{this.props.event ? this.props.event.title : "None"}</h1>
           </Modal.Header>
           <Modal.Body>
             <Form horizontal>
-              <p>{desc}</p>
+              <h3>Fag: </h3>
+              <p>testtesttest</p>
             </Form>
+            <Form>
+              <h3>Rom: </h3>
+              <p>fagfagfagfagfag</p>
+            </Form>
+            <Form>
+              <h3>Tidspunkt: </h3>
+              <p>Tidtiditditid</p>
+            </Form>
+            <iframe
+              src="http://use.mazemap.com/embed.html?https://use.mazemap.com/?v=1&campusid=1&left=10.4021537&right=10.4107368&top=63.4171325&bottom=63.4147558&zlevel=-1&sharepoitype=poi&sharepoi=2036&utm_medium=longurl"
+              width="100%"
+              height="420"
+              frameBorder="0"
+              marginHeight="0"
+              marginWidth="0"
+              scrolling="no">
+            </iframe>
           </Modal.Body>
           <Modal.Footer>
-            <Button onClick={() => this.close()} bsStyle="primary">Close</Button>
+            <Button bsStyle="success">Vis Kart</Button>
+            <Button  onClick={() => this.close()} bsStyle="primary">Lukk</Button>
           </Modal.Footer>
         </Modal>
       </div>
