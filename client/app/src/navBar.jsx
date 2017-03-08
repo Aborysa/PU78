1a8d9ee26b69f1eb3804934cad0ff0087294ec27
Jakob <jakob@westermoen.com>
2017-03-08 16:31:16 +0100

    fixed p-tag error, looks nicer now

--- client/app/src/navBar.jsx
+++ client/app/src/navBar.jsx
@@ -9,24 +9,28 @@ export class NavBar extends React.Component{
   }
   render() {
     let userNav = this.props.currentUser ?
-      <Nav bsStyle="pills"pullRight>
-        <NavDropdown eventKey={2} title="Fag" id="basic-nav-dropdown">
-          <MenuItem eventKey={2.1} href="/fghj">Action</MenuItem>
-          <MenuItem eventKey={2.2}>Another action</MenuItem>
-          <MenuItem eventKey={2.3}>Something else here</MenuItem>
-          <MenuItem divider />
-          <MenuItem eventKey={2.4}>Separated link</MenuItem>
-        </NavDropdown>
-        <Navbar.Text>
+      <Navbar.Collapse>
+        <Nav>
+          <NavDropdown eventKey={2} title="Fag" id="basic-nav-dropdown">
+            <MenuItem eventKey={2.1} href="/fghj">Action</MenuItem>
+            <MenuItem eventKey={2.2}>Another action</MenuItem>
+            <MenuItem eventKey={2.3}>Something else here</MenuItem>
+            <MenuItem divider />
+            <MenuItem eventKey={2.4}>Separated link</MenuItem>
+          </NavDropdown>
+        </Nav>
+        <Navbar.Text pullRight>
           Bruker: {this.props.currentUser.fullname}
         </Navbar.Text>
         <Navbar.Form pullRight>
           <Button href="/api/v1/logout" bsStyle="primary"><Glyphicon glyph="log-out" /> Logg ut</Button>
         </Navbar.Form>
-      </Nav>:
-      <Navbar.Form pullRight>
-        <Button href="/api/v1/login" bsStyle="primary"><Glyphicon glyph="user" /> Logg inn</Button>
-      </Navbar.Form>
+      </Navbar.Collapse>:
+      <Navbar.Collapse>
+        <Navbar.Form pullRight>
+          <Button href="/api/v1/login" bsStyle="primary"><Glyphicon glyph="user" /> Logg inn</Button>
+        </Navbar.Form>
+      </Navbar.Collapse>
 
     return (
       <div>
@@ -39,9 +43,7 @@ export class NavBar extends React.Component{
             </Navbar.Brand>
             <Navbar.Toggle />
           </Navbar.Header>
-          <Navbar.Collapse>
-            {userNav}
-          </Navbar.Collapse>
+          {userNav}
         </Navbar>
       </div>
     )
