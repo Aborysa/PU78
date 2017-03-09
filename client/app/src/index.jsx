ffd3bef7d623ad05fa9017cf83354e7925276954
Jakob <jakob@westermoen.com>
2017-03-09 12:28:05 +0100

    fixed react router error in console log, moved all routes to a const

--- client/app/src/index.jsx
+++ client/app/src/index.jsx
@@ -11,6 +11,14 @@ import { Router, Route, Link, IndexRoute, hashHistory, browserHistory } from 're
 import { userService } from 'services/user/user.service.js';
 
 
+const routes = (
+  <Route component={App}>
+    <Route path='/' component={LoginView} />
+    <Route path='/home' component={CalendarView} />
+    <Route path="*" component={NotFound} />
+  </Route>
+);
+
 class App extends React.Component{
   constructor() {
     super();
@@ -18,7 +26,6 @@ class App extends React.Component{
   }
   componentDidMount(){
     userService.getUser().subscribe(user => {
-      console.log(user);
       if(user && browserHistory.getCurrentLocation().pathname == "/"){
         browserHistory.push("/home");
       }else if(!user){
@@ -38,9 +45,7 @@ class App extends React.Component{
       <div>
         {nav}
         <Router history={browserHistory}>
-          <Route path='/' component={LoginView} />
-          <Route path='/home' component={CalendarView} />
-          <Route path="*" component={NotFound} />
+          {routes}
         </Router>
       </div>
     )
