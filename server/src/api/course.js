e56365ba515464879609f2aeec66982cfa529d4a
margreli <margreli@stud.ntnu.no>
2017-03-24 11:50:43 +0100

    added method to get courses from database based on search

---
+++ server/src/api/course.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const courseRouter = express.Router({mergeParams:true});
+const database = require("../database.js");
+
+courseRouter.get('/course', (req,res) => {
+  let search = req.body.search;
+  database.connect((conn, cb) => {
+    conn.query(
+      `SELECT * FROM Courses WHERE idCourse LIKE '%${search}%' LIMIT 10;`,
+      (_,rows) => {
+        if (!_) {
+          res.json(rows);
+        } else {
+          console.log(_);
+          res.json([1234]);
+        };
+      }
+    );
+  });
+});
+
+module.exports = courseRouter;
--- server/src/api/index.js
+++ server/src/api/index.js
@@ -6,6 +6,7 @@ let feide = require('./feide.js');
 let event = require('./event.js');
 let lecture = require('./lecture.js');
 let userCourses = require('./usercourses.js');
+let course = require('./course.js');
 
 //let r_odf = require('./odf.js');
 
@@ -13,6 +14,7 @@ api.api.use(feide);
 api.api.use(event);
 api.api.use(lecture);
 api.api.use(userCourses);
+api.api.use(course);
 
 module.exports = api;
 
