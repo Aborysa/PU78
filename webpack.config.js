ae6381ad04e8b21e794d0d4d673c559e092adc9a
Jarle Trollebø <aborysa@gmail.com>
2017-03-30 09:57:06 +0000

    Added style loader

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,9 @@ var config = {
   module : {
     loaders : [
       {
+        test: /\.css$/, loader: 'style!css'
+      },
+      {
         test: /\.test.jsx?$/,
         exclude: /node_modules/,
         loader: 'ignore-loader'
