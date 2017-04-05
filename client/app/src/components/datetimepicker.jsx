a6461097cc2acaa496892970e202b3a7cc396ebf
Jakob <jakob@westermoen.com>
2017-04-05 16:07:00 +0200

    fixed bugs in datepicker

--- client/app/src/components/datetimepicker.jsx
+++ client/app/src/components/datetimepicker.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Glyphicon } from 'react-bootstrap';
 
 export class DateTimePicker extends React.Component{
 	constructor(props){
@@ -11,8 +12,12 @@ export class DateTimePicker extends React.Component{
 			todayHighlight: true,
 			fontAwesome: true,
 			startDate: new Date()
+		}).on('changeDate',(ev)=>{
+			this.handleChange(ev.date)
 		});
+
 	}
+
   handleChange(text){
     this.props.onChange(text);
     console.log(text);
@@ -20,8 +25,10 @@ export class DateTimePicker extends React.Component{
 	render(){
 		return(
 			<div className="input-append date" ref="picker" data-date="12-02-2012" data-date-format="dd/mm/yyyy hh:ii">
-				<input onChange={(v)=>this.handleChange(v)} className="form-control" size="16" type="text"/>
-				<span className="add-on"><i className="icon-th"></i></span>
+				<div className="input-group mb-2 mr-sm-2 mb-sm-0">
+					<input className="form-control" size="16" type="text" readOnly/>
+					<span className="input-group-addon"><Glyphicon glyph="th"/></span>
+				</div>
 			</div>
 		)
 	}
